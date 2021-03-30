2fdc7400cd185e6c9f285c87bf9e05a20b4c13b0
sebastiansandoval27 <juan.sandovals@usantoto.edu.co>
2021-03-29 21:05:06 -0500

    Chat messages sample added

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Chat Flutter App',
       debugShowCheckedModeBanner: false,
-      initialRoute: 'login',
+      initialRoute: 'chat',
       routes: appRoutes,
     );
   }
